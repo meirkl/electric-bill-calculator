d3742bffa6b93a535e83bd49b3082c0b832001a1
Meir <mrzkeller@gmail.com>
2019-11-03 01:26:31 +0200

    Updates

---
+++ precache-manifest.9f2584cbd8906a882b20e60cc2e03bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6c6edb559d10e8c30e6aa74a6cc872",
+    "url": "/electric-bill-calculator/index.html"
+  },
+  {
+    "revision": "73a4bb2cf57b481a04a3",
+    "url": "/electric-bill-calculator/static/js/2.1fd405ae.chunk.js"
+  },
+  {
+    "revision": "de05a4740cc1eee9e54f",
+    "url": "/electric-bill-calculator/static/js/main.dd50f507.chunk.js"
+  },
+  {
+    "revision": "97af4b462abd30c43f2c",
+    "url": "/electric-bill-calculator/static/js/runtime-main.d0568011.js"
+  },
+  {
+    "revision": "975cb155eeb32dd35521d0149902df8e",
+    "url": "/electric-bill-calculator/static/media/settings.975cb155.svg"
+  }
+]);
